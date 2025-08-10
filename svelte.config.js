90ce11438a697e0e2bfe5a0ae71501a371c4461e
Widoyo <widoyo@gmail.com>
2025-08-10 15:37:12 +0700

    Migrate to Cloudflare Worker

--- svelte.config.js
+++ svelte.config.js
@@ -6,22 +6,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({
-			config: undefined,
-			platformProxy: {
-				configPath: undefined,
-				environment: undefined,
-				persist: undefined
-			},
-			fallback: 'plaintext',
-			routes: {
-				include: ['/*'],
-				exclude: ['<all>']
-			}
-		}),
-		prerender: {
-			handleHttpError: 'warn'
-		}
+		adapter: adapter(),
 	}
 };
 
