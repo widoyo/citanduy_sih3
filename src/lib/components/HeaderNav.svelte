7f035d80d93eb7e4209b1b1fcb60495357a490f3
Widoyo <widoyo@gmail.com>
2025-10-31 06:27:43 +0700

    Perbesar font size pada Menu

--- src/lib/components/HeaderNav.svelte
+++ src/lib/components/HeaderNav.svelte
@@ -3,7 +3,7 @@
     import { GlobeOutline, ChevronDownOutline, BellRingOutline } from "flowbite-svelte-icons";
 </script>
 
-<Navbar class="fixed start-0 top-0 z-20 w-full px-2 py-2 backdrop-blur-sm">
+<Navbar class="fixed start-0 top-0 z-20 w-full px-2 py-2 backdrop-blur-sm text-lg">
 	<NavBrand href="/" class="flex items-center mb-1">
 		<span class="font-extrabold me-3">SIH3</span> WS Citanduy
 	</NavBrand>
@@ -13,19 +13,19 @@
 		<NavHamburger />
 	</div>
 	<NavUl>
-		<NavLi href="/" class="text-gray-900 dark:text-white">
+		<NavLi href="/" class="text-lg font-normal text-gray-900 dark:text-white">
 			Home
 		</NavLi>
-		<NavLi href="/#hidrologi" class="text-gray-900 dark:text-white">
+		<NavLi href="/#hidrologi" class="text-lg font-normal text-gray-900 dark:text-white">
 			Hidrologi
 		</NavLi>
-		<NavLi href="/#meteorologi" class="text-gray-900 dark:text-white">
+		<NavLi href="/#meteorologi" class="text-lg font-normal text-gray-900 dark:text-white">
 			Hidrometeorologi
 		</NavLi>
-		<NavLi href="/#hidrogeologi" class="text-gray-900 dark:text-white">
+		<NavLi href="/#hidrogeologi" class="text-lg font-normal text-gray-900 dark:text-white">
 			Hidrogeologi
 		</NavLi>
-		<NavLi>About<ChevronDownOutline class="text-primary-800 ms-2 inline h-6 w-6 dark:text-white" />
+		<NavLi class="text-lg font-normal">About<ChevronDownOutline class="text-lg font-normal text-primary-800 ms-2 inline h-6 w-6 dark:text-white" />
     </NavLi>
     <Dropdown simple class="w-44">
       <DropdownItem href="/#mitra">Mitra</DropdownItem>
